37f57063075b73e1f9d8518906a79e2865727a9a
Albert <albert_henry@outlook.com>
2021-03-03 19:57:48 +0700

    add scripts to define HF

---
+++ scripts/define_hf.R
@@ -0,0 +1,65 @@
+# exploratory data analysis to preprocess data
+
+pacman::p_load(tidyverse, vroom, magrittr, lubridate)
+
+# list of df
+
+list_file <- list(
+  baseline = "resources/data/baseline_subset.txt",
+  pheno_308 = "resources/data/comorbidities_308_defined.txt",
+  death = "resources/data/death_subset.txt",
+  hes = "resources/data/hes_subset.txt",
+  gp = "resources/data/gp_clinical_subset.txt",
+  oper = "resources/data/operation_subset.txt"
+)
+
+list_df <- map(list_file, vroom, col_select=-1)
+
+list_df_code <- readxl::read_excel("resources/code_list/2021-01-19_clean_pheno.xlsx",
+                              sheet = "ALL") %>%
+  select(-code) %>% 
+  group_by(dict) %>% 
+  nest %>% 
+  deframe
+
+# function to join and summarise based on earliest occurence
+join_sum <- function(df, fn_date, col_date, col_code, dict){
+  col_code <- enquo(col_code)
+  col_date <- enquo(col_date)
+  df %>% 
+    filter(!is.na(!!col_date)) %>% 
+    mutate(date_diag = fn_date(!!col_date)) %>% 
+    select(eid, date_diag, code = !!col_code) %>% 
+    inner_join(list_df_code[[dict]],
+               by = c(code = "code_clean")) %>%
+    mutate(dict = dict) %>% 
+    group_by(eid, pheno) %>% 
+    filter(date_diag == min(date_diag, na.rm = T))
+}
+
+df_diag <- bind_rows(
+  list_df$hes %>% join_sum(ymd, admidate, icd10, "icd10"),
+  list_df$gp %>% join_sum(ymd, event_dt, read_code, "read"),
+  list_df$oper %>% join_sum(ymd, opdate, oper4, "opcs")
+)
+
+df_basedate <- list_df$baseline %>% 
+  filter(field == 53, i == 0, n == 0) %>% 
+  mutate(date_attending = ymd(value)) %>% 
+  select(eid, date_attending) %>% 
+  distinct(eid, .keep_all = T)
+
+df_death_all <- vroom("resources/data/death.txt.gz") %>% 
+  mutate(date_of_death = dmy(date_of_death)) %>% 
+  select(eid, date_of_death)
+
+
+  
+
+
+
+
+
+
+
+
---
+++ scripts/survival_analysis.R
@@ -0,0 +1,26 @@
+# Survival analysis
+# end of follow up (based on latest data freeze date in UKB)
+# https://biobank.ndph.ox.ac.uk/ukb/exinfo.cgi?src=Data_providers_and_dates
+# may need to create separate end date for different providers as they have
+# different data freeze date
+library(survival)
+
+
+date_censored <- ymd("2020-11-30")
+
+make_df_surv <- function()
+  
+  
+  df_surv <- df_diag %>% 
+  filter(date_diag == min(date_diag)) %>% 
+  inner_join(df_basedate, by = "eid") %>% 
+  left_join(df_death_all, by = "eid")
+
+df_surv %>% 
+  # exclude prevalent cases + incident cases within 1st year after recruitment
+  filter(date_diag > date_attending + dyears(1)) %>% 
+  # define censored (death == 0) or death (death == 1) status based on presence in death registry
+  mutate(death = ifelse(is.na(date_of_death, 0, 1)),
+         time = ifelse(death == 1,
+                       date_of_death - date_attending,
+                       date_censored - date_attending) %>% as.numeric) 
